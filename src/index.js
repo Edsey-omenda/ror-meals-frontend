e60d1770c022b8c9bfb90559802e5d6cd8ee67de
Edsey-omenda <omendajames0317@gmail.com>
2023-04-20 15:32:33 +0300

    updates the index js by using redux to hold state

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/app/App";
+import "./index.css";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import menu_itemsReducer, {
+  menu_itemsFetch,
+} from "./components/homeuser/ProductSlice";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { menu_itemsApi } from "./components/homeuser/ProductsApi";
+import cartReducer, { getTotals }  from "./components/homeuser/CartSlice";
+import userReducer, {getUser} from './components/homeuser/UserSclice'
+
+const store = configureStore({
+  //check what we have in state
+  reducer: {
+    menu_items: menu_itemsReducer,
+    cart: cartReducer,
+    user: userReducer,
+
+    [menu_itemsApi.reducerPath]: menu_itemsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(menu_itemsApi.middleware),
+});
+
+store.dispatch(menu_itemsFetch());
+store.dispatch(getTotals());
+store.dispatch(getUser());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-
